214ee4446ea5ada7839c0727daf184a4864d15ba
Hilal Silin <hilal.silin25@gmail.com>
2025-03-18 14:41:58 +0300

    aaa

--- OtelRezervasyon/Form1.cs
+++ OtelRezervasyon/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace OtelRezervasyon
 {
     public partial class Form1 : Form
@@ -6,5 +9,37 @@ namespace OtelRezervasyon
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btngiriþ_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(); // Form2 nesnesi oluþtur
+            form2.Show(); // Form2'yi aç
+            this.Hide(); // Form1'i gizle (eðer tamamen kapanmasýný istiyorsan this.Close() kullanabilirsin)
+        }
     }
+
+
+
+
 }
---
+++ OtelRezervasyon/Form2.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace OtelRezervasyon
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            texttelefonara = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveBorder;
+            panel1.Controls.Add(texttelefonara);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1141, 98);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Telefon ara:";
+            // 
+            // texttelefonara
+            // 
+            texttelefonara.Location = new Point(108, 24);
+            texttelefonara.Name = "texttelefonara";
+            texttelefonara.Size = new Size(147, 27);
+            texttelefonara.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1141, 622);
+            Controls.Add(panel1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox texttelefonara;
+        private Label label1;
+    }
+}
